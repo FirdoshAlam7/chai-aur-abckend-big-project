7a83f23e2593a11de4ade3ee26c3e74bb7afc00c
FirdoshAlam7 <110092059+FirdoshAlam7@users.noreply.github.com>
2024-04-19 14:07:03 +0530

    final config setting for project and add multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+const uploadCloudinary = async function (localFilePath) {
+  try {
+    if (!localFilePath) return null;
+    //*upload the file to the cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //* file has been successfully uploaded
+    console.log("File successfully uploaded for cloudinary", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) //* remove the locally saved temporary file as the upload operation got failed 
+    return null;
+  }
+};
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_COULD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+export { uploadCloudinary};
